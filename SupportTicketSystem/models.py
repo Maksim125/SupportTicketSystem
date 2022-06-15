bfc9ad14bb2c6332c62e39ea0e47201507074212
max <maxyarmakovich@gmail.com>
2022-06-14 20:03:16 -0400

    create schemas for the website data

--- SupportTicketSystem/models.py
+++ SupportTicketSystem/models.py
@@ -1,17 +1,9 @@
+from enum import unique
 from sqlalchemy import ForeignKey
 from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
-
-
-class Note(db.Model):
-    '''
-    Note schema
-    '''
-    id = db.Column(db.Integer, primary_key = True)
-    data = db.Column(db.String(10000))
-    date = db.Column(db.DateTime(timezone = True), default = func.now())
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+from datetime import datetime
 
 
 class User(db.Model, UserMixin):
@@ -19,7 +11,57 @@ class User(db.Model, UserMixin):
     User schema
     '''
     id = db.Column(db.Integer, primary_key = True)
-    email = db.Column(db.String(150), unique = True)
-    password = db.Column(db.String(150))
-    first_name = db.Column(db.String(150))
-    notes = db.relationship("Note")
+    email = db.Column(db.String(150), unique = True, nullable = False)
+    password = db.Column(db.String(100), unique = True, nullable = False)
+    username = db.Column(db.String(50))
+
+    tickets = db.relationship("Ticket", backref = "author", lazy = True) #tickets made by the user
+    comments = db.relationship("Comment", backref = "author") #comments made by the user
+
+
+class Ticket(db.Model):
+    '''
+    Ticket Schema
+    '''
+    id = db.Column(db.Integer, primary_key = True)
+
+    time_posted = db.Column(db.DateTime, nullable = False, default = datetime.utcnow)
+    time_resolved = db.Column(db.DateTime, nullable = True) #Null if not yet resolved
+
+    title = db.Column(db.String(120), nullable = False)
+    content = db.Column(db.Text, nullable = False)
+
+    resolved = db.Column(db.Boolean, nullable = False, default = False)
+    priority = db.Column(db.Integer, nullable = False, default = 0) #Default low priority
+
+    comments = db.relationship("Comment", backref = "ticket", lazy = True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable = False) #id of author
+    group_id = db.Column(db.Integer, db.ForeignKey("groups.id"), nullable = True) #group the ticket belongs to
+
+
+class Comment(db.Model):
+    '''
+    Comment schema
+    '''
+    id = db.Column(db.Integer, primary_key = True)
+    time_posted = db.Column(db.DateTime, nullable = False, default = datetime.utcnow)
+    content = db.Column(db.Text, nullable = False)
+    ticket_id = db.Column(db.Integer, db.ForeignKey("ticket.id"), nullable = False) #id of ticket the comment is for
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable = False) #id of the user who made the comment
+
+
+class Groups(db.Model):
+    '''
+    Groups schema
+    '''
+    id = db.Column(db.Integer, primary_key = True)
+    group_name = db.Column(db.String(100), unique = True)
+
+
+class User_Groups(db.Model):
+    '''
+    User groups schema
+    '''
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), primary_key = True)
+    group_id = db.Column(db.String(50), db.ForeignKey("user.id"), primary_key = True)
+    rank_in_group = db.Column(db.Integer, nullable = False, default = 0)
